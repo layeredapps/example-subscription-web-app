d8db1f13d291785417341c773020694dc1b487de
beninsydney <benodev@protonmail.com>
2022-04-04 21:04:52 +1000

    WIP

--- application-server/start-dev.sh
+++ application-server/start-dev.sh
@@ -3,8 +3,8 @@ if [ ! -d node_modules ]; then
   npm install
 fi
 APPLICATION_SERVER_TOKEN="this is the token" \
-APPLICATION_SERVER="http://localhost:8200" \
-DASHBOARD_SERVER="localhost:8882" \
-APPLICATION_SERVER_PORT=8200 \
-HOST=0.0.0.0 \
+APPLICATION_SERVER="http://localhost:8300" \
+DASHBOARD_SERVER="http://localhost:8200" \
+APPLICATION_SERVER_PORT=8300 \
+PORT=8200 \
 node main.js 
