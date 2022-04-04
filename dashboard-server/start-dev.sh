3bab792b27b9bce1bcf987a105f978b27038e850
beninsydney <benodev@protonmail.com>
2022-04-05 07:17:38 +1000

    WIP

--- dashboard-server/start-dev.sh
+++ dashboard-server/start-dev.sh
@@ -3,6 +3,6 @@ APPLICATION_SERVER="http://localhost:8300" \
 DASHBOARD_SERVER="http://localhost:8200" \
 STRIPE_JS="false" \
 SUBSCRIPTION_WEBHOOK_ENDPOINT_SECRET="asdf" \
-PORT=8300 \
+PORT=8200 \
 IP=0.0.0.0 \
 node main.js 
