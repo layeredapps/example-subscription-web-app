64da2889ee0392f81b57e5f3e8693fb840582bed
beninsydney <benodev@protonmail.com>
2022-04-28 11:29:50 +1000

    Cleaned up

--- dashboard-server/tests.sh
+++ dashboard-server/tests.sh
@@ -10,4 +10,4 @@ APPLICATION_SERVER_PORT=4000 \
 SUBSCRIPTIONS_WEBHOOK_ENDPOINT_SECRET="asdf" \
 SUBSCRIPTIONS_STRIPE_PUBLISHABLE_KEY=$CONNECT_STRIPE_PUBLISHABLE_KEY \
 SUBSCRIPTIONS_STRIPE_KEY=$CONNECT_STRIPE_KEY \
-node --expose-gc --max-old-space-size=2048 ./node_modules/.bin/mocha --sort true --file test-helper.js --timeout 480000 --ignore 'node_modules/hpagent/**/*' --slow 480000 --recursive --extension .test.js . 2>&1 | tee tests.txt
+mocha --file test-helper.js --timeout 480000 --ignore 'node_modules/hpagent/**/*' --slow 480000 --recursive --extension .test.js --retries 2 . 2>&1 | tee tests.txt
