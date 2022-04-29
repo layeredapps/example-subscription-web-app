d308bd6febaba0892ab29048f9ae6449c432ef2f
beninsydney <benodev@protonmail.com>
2022-04-29 19:39:28 +1000

    Don't generate screenshots

--- dashboard-server/tests.sh
+++ dashboard-server/tests.sh
@@ -1,6 +1,4 @@
 NODE_ENV=testing \
-GENERATE_SCREENSHOTS=true \
-SCREENSHOT_PATH=/home/appstore-project/workspace/dashboard/layeredapps.github.io/screenshots/example-subscription-web-app \
 PORT=9000 \
 APPLICATION_SERVER=http://localhost:4000 \
 APPLICATION_SERVER_TOKEN=token \
